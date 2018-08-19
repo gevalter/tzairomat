e50a425cf41ec71122c52306713ad9990f036cb9
gevalter <geva.alter1995@gmail.com>
2018-08-19 23:59:04 +0000

    Validators to home page

--- client/js/site.js
+++ client/js/site.js
@@ -127,6 +127,11 @@ TzairJS.controller("main", function($scope,$http) {
   // asked question div is disappeared
   $scope['question_div'] = "panel-group disappeared";
   
+  // Initialize the ngmodels that need validation - ngIf will not work on startup
+  $scope.add_que_modal_syntax = "נוסח";
+  $scope.add_que_modal_answer = "תשובה";
+  $scope.add_topic_text = "שם נושא";
+  
   // Topics query
   $http.get('/topics')
   .success(function(data){
@@ -217,47 +222,129 @@ TzairJS.controller("main", function($scope,$http) {
 
   // Save a topic to db
   $scope.save_topic = function(){
-    var topic_details = {
-      name: $scope.add_topic_text,
-      questions: []
-    };
-    
-    var topic_post = $.post("/addtopic", { topic_details });
-    
-    // Send error / success message
-    topic_post.done(function( data ) {
-      alert(data);
-    });
-    
-    // Reset the textbox
-    $scope.add_topic_text = null;
+    // Check if there is a name for the topic
+    if ($scope.add_topic_text.length > 0) 
+    {
+      // Init boolean for checking form errors - increasing every error 
+      var addtopic_form_errors_counter = 0;
+      
+      // Case 1 - User didn't click any topics
+      if ($scope.add_topic_text === "שם נושא")
+      {
+        addtopic_form_errors_counter++;
+        var topic_name_error = angular.element("<div id='default_topic_name_error' class='add_que_modal_width alert alert-danger'><strong>אנא שנה שם נושא</strong></div>")
+        var topic_error_div = document.getElementById('default_topic_name_div');
+        
+        // Find if this error is already exist in DOM
+        try {
+          angular.element(default_topic_name_error).length}
+        catch(err){
+          angular.element(topic_error_div).append(topic_name_error);
+        }
+      }
+      
+      // Check for error counter
+      if (addtopic_form_errors_counter === 0)
+      {
+        var topic_details = {
+          name: $scope.add_topic_text,
+          questions: []
+        };
+        
+        var topic_post = $.post("/addtopic", { topic_details });
+        
+        $('#addtopicbtnClose').click();
+        
+        // Send error / success message
+        topic_post.done(function( data ) {
+          alert(data);
+        });
+        
+        // Reset the textbox
+        $scope.add_topic_text = null;
+      }
+    }
   };
   
   // Save a question to db
   $scope.save_question = function(){
+    
+    // Build the question object for send
     var question_details = {
       syntax: $scope.add_que_modal_syntax,
       answer: $scope.add_que_modal_answer,
       topics: topics_clicked
       };
       
-      
-    //Send the data using post
-    var que_post = $.post("/addque", { question_details });
+    // Init boolean for checking form errors - increasing every error 
+    var form_errors_counter = 0;
     
-    // Send error / success message
-    que_post.done(function( data ) {
-      alert(data);
-    });
-    
-    // Reset question details
-    $scope.add_que_modal_answer = null;
-    $scope.add_que_modal_syntax = null;
-    topics_clicked = null;
-    
-    for(var topics_loop = 0; topics_loop < $scope.topics.length; topics_loop++)
+    // Case 1 - User didn't click any topics
+    if (question_details.topics.length === 0)
     {
-      $scope['topic_button_' + topics_loop] = null;
+      form_errors_counter++;
+      var topics_error = angular.element("<div id='no_topics_error' class='add_que_modal_width alert alert-danger'><strong>אנא הכנס נושא</strong></div>")
+      var error_div = document.getElementById('topics_error_div');
+      
+      //Find if this error is already exist in DOM
+      try {
+        angular.element(no_topics_error).length}
+      catch(err){
+        angular.element(error_div).append(topics_error);}
+    }
+    
+    // Case 2 - User didn't changed syntax input in form
+    if(question_details.syntax === "נוסח")
+    {
+      form_errors_counter++; 
+      var syntax_error = angular.element("<div id='default_syntax_error' class='add_que_modal_width alert alert-danger'><strong>אנא שנה ניסוח שאלה</strong></div>")
+      var error_div = document.getElementById('default_syntax_error_div');
+      
+      // Find if this error is already exist in DOM 
+      try {
+        angular.element(default_syntax_error).length}
+      catch(err){
+        angular.element(error_div).append(syntax_error);}
+    }
+    
+    // Case 3 - User didn't changed answer input in form
+    if (question_details.answer === "תשובה")
+    {
+      form_errors_counter++;
+      var answer_error = angular.element("<div id='default_answer_error' class='add_que_modal_width alert alert-danger'><strong>אנא שנה תשובה</strong></div>")
+      var error_div = document.getElementById('default_answer_error_div');
+      
+      // Find if this error is already exist in DOM 
+      try {
+        angular.element(default_answer_error).length}
+      catch(err){
+        angular.element(error_div).append(answer_error);}
+    }
+    
+    // Checking error counter
+    if (form_errors_counter === 0 && ($scope.add_que_modal_syntax != undefined) && $scope.add_que_modal_answer != undefined)
+    {
+      // Send the data using post
+      var que_post = $.post("/addque", { question_details });
+      
+      // Send error / success message
+      que_post.done(function( data ) {
+        alert(data);
+      });
+      
+      // Close the modal - by manually clicking the close button
+      $('#addquebtnClose').click();
+      
+      // Reset question details
+      $scope.add_que_modal_answer = null;
+      $scope.add_que_modal_syntax = null;
+      topics_clicked = null;
+      
+      // Reset topic buttons
+      for(var topics_loop = 0; topics_loop < $scope.topics.length; topics_loop++)
+      {
+        $scope['topic_button_' + topics_loop] = null;
+      }
     }
   };
 });
